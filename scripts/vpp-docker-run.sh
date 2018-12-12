895e3cf87c52358162a0e95a97b4509eb51755bd
Koichiro Den <den@klaipeden.com>
2018-12-12 14:26:59 +0900

    [SO-PERSONAL] 8)

--- scripts/vpp-docker-run.sh
+++ scripts/vpp-docker-run.sh
@@ -7,7 +7,12 @@
 scriptpath=$GOPATH/src/github.com/containernetworking/cni/scripts
 echo $scriptpath
 
-contid=$(docker run -d --net=none vpp-centos-userspace-cni:latest /bin/sleep 10000000)
+contid=$(docker run -d --net=none k8s.gcr.io/pause /bin/sleep 10000000)
+
+# I want it to be thoroughly under my control :p
+#/root/vpp/build-root/build-vpp_debug-native/vpp/bin/vpp -c /etc/vpp/startup.conf &> /var/log/vpp/boot.log &
+docker run -d -v /var/run/vpp/cni/shared:/var/run/vpp/cni/shared:rw -v /var/run/vpp/cni/$contid:/var/run/vpp/cni/data:rw -v /dev/shm:/dev/shm --privileged --net=host vpp-centos-userspace-cni:latest
+
 pid=$(docker inspect -f '{{ .State.Pid }}' $contid)
 netnspath=/proc/$pid/ns/net
 
@@ -20,5 +25,4 @@ function cleanup() {
 trap cleanup EXIT
 
 #docker run -v /var/run/vpp/:/var/run/vpp:rw --device=/dev/hugepages:/dev/hugepages --net=container:$contid $@
-docker run -v /var/run/vpp/cni/shared:/var/run/vpp/cni/shared:rw -v /var/run/vpp/cni/$contid:/var/run/vpp/cni/data:rw --device=/dev/hugepages:/dev/hugepages --net=container:$contid $@
-
+docker run -v /var/run/vpp/cni/shared:/var/run/vpp/cni/shared:rw -v /var/run/vpp/cni/$contid:/var/run/vpp/cni/data:rw -v /dev/hugepages:/dev/hugepages --net=container:$contid $@
